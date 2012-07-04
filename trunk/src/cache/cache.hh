944c73d54bf1292cc72cb7087b47e55360d6277e
Wilson Li <windkit@cse.cuhk.edu.hk>
2012-07-04 17:27:31 +0800

    Added Cache Miss Exception Declaration

--- trunk/src/cache/cache.hh
+++ trunk/src/cache/cache.hh
@@ -2,13 +2,22 @@
 #define __CACHE_HH__
 
 #include <stdint.h>
+#include <exception>
+using namespace std;
+
+class CacheMissException : public exception {
+	virtual const char* what() const throw()
+	{
+		return "Cache Miss";
+	}
+} CacheMiss;
 
 class Cache {
-public:
-	uint32_t readCache (uint32_t id);
-	uint32_t writeCache (uint32_t id, char data[]);
-	uint32_t createEntry (uint32_t id);
-	uint32_t deleteEntry (uint32_t id);
-private:
+	public:
+		uint32_t read (uint32_t id);
+		uint32_t write (uint32_t id, char* data);
+		uint32_t createEntry (uint32_t id);
+		uint32_t deleteEntry (uint32_t id);
+	private:
 };
 #endif
