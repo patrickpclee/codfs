7ba80eb491dc5db404bc798ba47f25ffdb5766d6
Jeremy Chan <cwchan8@cse.cuhk.edu.hk>
2012-07-04 14:25:52 +0800

    updated makefile and fixed communicator includes

--- trunk/src/client/client_communicator.hh
+++ trunk/src/client/client_communicator.hh
@@ -1,6 +1,8 @@
 #ifndef __CLIENT_COMMUNICATOR_HH__
 #define __CLIENT_COMMUNICATOR_HH__
 
+#include "../communicator/communicator.hh"
+
 class ClientCommunicator : public Communicator {
 public:
 	void display ();
--- trunk/src/osd/osd_communicator.hh
+++ trunk/src/osd/osd_communicator.hh
@@ -1,6 +1,8 @@
 #ifndef __OSD_COMMUNICATOR_HH__
 #define __OSD_COMMUNICATOR_HH__
 
+#include "../communicator/communicator.hh"
+
 class OsdCommunicator : public Communicator {
 public:
 	void display ();
