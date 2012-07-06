d8a9caeaa8fb1a039b1ea5f0196184610d952b3f
Wilson Li <windkit@cse.cuhk.edu.hk>
2012-07-06 17:46:12 +0800

    casting uint64_t to long long for mongodb connect

--- trunk/src/mds/mongodb.cc
+++ trunk/src/mds/mongodb.cc
@@ -8,4 +8,13 @@ uint32_t MongoDB::connect(){
 	_connection.connect(_host);
 }
 
+void MongoDB::read(string table, uint64_t id, vector<uint64_t>data){
+	string targetTable = COLLECTION + (string)"." + table;
+	auto_ptr<DBClientCursor> cursor =
+		_connection.query(targetTable, QUERY( "id" << (long long)id ) );
+	while( cursor->more() ) {
+		BSONObj p = cursor->next();
+		cout << p.getStringField("name") << endl;
+	}
 
+}
--- trunk/src/mds/mongodb.hh
+++ trunk/src/mds/mongodb.hh
@@ -1,6 +1,7 @@
 #ifndef __MONGO_DB_HH__
 #define __MONGO_DB_HH__
 
+#define COLLECTION "ncvfs"
 #include "mongo/client/dbclient.h"
 using namespace mongo;
 
@@ -10,6 +11,7 @@ class MongoDB {
 public:
 	MongoDB (string host);
 	uint32_t connect();
+	void read(string table, uint64_t id, vector<uint64_t> data);
 private:
 	string _host;
 	DBClientConnection _connection;
