877dc03929cfc2243821583c675d71c326082b4b
Wilson Li <windkit@cse.cuhk.edu.hk>
2012-07-12 15:17:40 +0800

    Added Dummy Cache Header

---
+++ trunk/src/cache/dummycache.hh
@@ -0,0 +1,11 @@
+#ifndef __DUMMY_CACHE_HH__
+#define __DUMMY_CACHE_HH__
+
+#define "cache.hh"
+
+class DummyCache : public Cache {
+	public:
+	private:
+};
+
+#endif
--- trunk/src/cache/objectmetadatacache.cc
+++ trunk/src/cache/objectmetadatacache.cc
@@ -1,17 +1,1 @@
 #include "objectmetadatacache.hh"
-
-uint32_t ObjectMetaDataCache::readObjectMetaData (uint64_t objectId){
-	throw new CacheMissException();
-}
-
-uint32_t ObjectMetaDataCache::writeObjectMetaData (uint64_t objectId, ObjectMetaData objectMetaData){
-	throw new CacheMissException();
-}
-
-uint32_t ObjectMetaDataCache::createObjectMetaData (uint64_t objectId){
-	return 0;
-}
-
-uint32_t ObjectMetaDataCache::deleteObjectMetaData (uint64_t objectId){
-	return 0;
-}
