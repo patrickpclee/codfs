b4ffda04b036ddbcda27eb1c76c9db6dc6c4d7b4
Wilson Li <windkit@cse.cuhk.edu.hk>
2012-07-12 15:26:51 +0800

    Added Dummy Cache

--- trunk/src/cache/cache.hh
+++ trunk/src/cache/cache.hh
@@ -17,7 +17,7 @@ class Cache {
 		uint32_t read (uint32_t id);
 		uint32_t write (uint32_t id, char* data);
 		uint32_t createEntry (uint32_t id);
-		uint32_t deleteEntry (uint32_t id);
+		void deleteEntry (uint32_t id);
 	private:
 };
 #endif
