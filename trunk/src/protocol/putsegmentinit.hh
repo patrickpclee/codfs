075306d51d496b718747892d61480f64cf361823
chintran <chintran@live.cn>
2012-08-03 17:39:52 +0800

    Segment_Msg/Put_Segment_Init_Msg added.

---
+++ trunk/src/protocol/putsegmentinit.hh
@@ -0,0 +1,57 @@
+#ifndef __PUTOBJECTINITREQUEST_HH__
+#define __PUTOBJECTINITREQUEST_HH__
+
+#include "message.hh"
+
+using namespace std;
+
+/**
+ * Extends the Message class
+ * Initiate an segment transfer
+ */
+
+class PutSegmentInitMsg: public Message {
+public:
+
+	PutSegmentInitMsg(Communicator* communicator);
+
+	PutSegmentInitMsg(Communicator* communicator, uint32_t osdSockfd,
+			uint64_t objectId, uint32_t segmentId, uint32_t segmentSize, uint32_t chunkCount);
+
+	/**
+	 * Copy values in private variables to protocol message
+	 * Serialize protocol message and copy to private variable
+	 */
+
+	void prepareProtocolMsg();
+
+	/**
+	 * Override
+	 * Parse message from raw buffer
+	 * @param buf Raw buffer storing header + protocol + payload
+	 */
+
+	void parse(char* buf);
+
+	/**
+	 * Override
+	 * Execute the corresponding Processor
+	 */
+
+	void handle();
+
+	/**
+	 * Override
+	 * DEBUG: print protocol message
+	 */
+
+	void printProtocol();
+
+private:
+	uint64_t _objectId;
+	uint32_t _segmentId;
+	uint32_t _segmentSize;
+	uint32_t _chunkCount;
+};
+
+#endif
---
+++ trunk/src/protocol/segmentdatamsg.hh
@@ -0,0 +1,60 @@
+#ifndef __OBJECTDATAMSG_HH__
+#define __OBJECTDATAMSG_HH__
+
+#include "message.hh"
+
+using namespace std;
+
+/**
+ * Extends the Message class
+ * Initiate an segment transfer
+ */
+
+class SegmentDataMsg: public Message {
+public:
+
+	SegmentDataMsg(Communicator* communicator);
+
+	SegmentDataMsg(Communicator* communicator, uint32_t dstSockfd,
+			uint64_t objectId,
+			uint32_t segmentId,
+			uint64_t offset,
+			uint32_t length);
+
+	/**
+	 * Copy values in private variables to protocol message
+	 * Serialize protocol message and copy to private variable
+	 */
+
+	void prepareProtocolMsg();
+
+	/**
+	 * Override
+	 * Parse message from raw buffer
+	 * @param buf Raw buffer storing header + protocol + payload
+	 */
+
+	void parse(char* buf);
+
+	/**
+	 * Override
+	 * Execute the corresponding Processor
+	 */
+
+	void handle();
+
+	/**
+	 * Override
+	 * DEBUG: print protocol message
+	 */
+
+	void printProtocol();
+
+private:
+	uint64_t _objectId;
+	uint32_t _segmentId;
+	uint64_t _offset;
+	uint32_t _length;
+};
+
+#endif
