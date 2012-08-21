90d3140098147dc711532de7ce86a796641a00bb
Wilson Li <windkithk@gmail.com>
2012-08-21 15:00:32 +0800

    [FUSE] Started to Build Fuse Client

---
+++ trunk/src/fuse/client_fuse.cc
@@ -0,0 +1,67 @@
+#define FUSE_USE_VERSION 26
+
+#include <fuse.h>
+#include <errno.h>
+
+#include "client.hh"
+#include "../config/config.hh"
+
+Client* client;
+
+ConfigLayer* configLayer;
+
+static int ncvfs_open(const char *path, struct fuse_file_info *fi)
+{
+	(void) fi;
+
+	if(strcmp(path, "/") != 0)
+		return -ENOENT;
+
+	return 0;
+}
+
+static int ncvfs_read(const char *path, char *buf, size_t size,
+		     off_t offset, struct fuse_file_info *fi)
+{
+	(void) buf;
+	(void) offset;
+	(void) fi;
+
+	if(strcmp(path, "/") != 0)
+		return -ENOENT;
+
+	if (offset >= (1ULL << 32))
+		return 0;
+
+	return size;
+}
+
+static int ncvfs_write(const char *path, const char *buf, size_t size,
+		      off_t offset, struct fuse_file_info *fi)
+{
+	(void) buf;
+	(void) offset;
+	(void) fi;
+
+	if(strcmp(path, "/") != 0)
+		return -ENOENT;
+
+	return size;
+}
+
+struct ncvfs_fuse_operations: fuse_operations
+{
+	ncvfs_fuse_operations ()
+	{
+		open	= ncvfs_open;
+		read	= ncvfs_read;
+		write	= ncvfs_write;
+	}
+};
+
+static struct ncvfs_fuse_operations ncvfs_oper;
+
+int main(int argc, char *argv[])
+{
+	return fuse_main(argc, argv, &ncvfs_oper, NULL);
+}
