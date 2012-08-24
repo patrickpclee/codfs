411cedef59ccc264025b3c8b88b35fd7e577b0f9
Jeremy Chan <cwchan@cse.cuhk.edu.hk>
2012-08-24 11:11:33 +0800

    moved #include "XXX.hh" inside #ifdef for transfer and status messages
    Former-commit-id: 79264dec1d91cc0b7e50f547377f91045ac70a03

--- trunk/src/protocol/transfer/putsegmentinitreply.cc
+++ trunk/src/protocol/transfer/putsegmentinitreply.cc
@@ -4,7 +4,6 @@
 #include "../../protocol/message.pb.h"
 #include "../../common/enums.hh"
 #include "../../common/memorypool.hh"
-#include "../../osd/osd.hh"
 
 PutSegmentInitReplyMsg::PutSegmentInitReplyMsg(Communicator* communicator) :
 		Message(communicator) {
