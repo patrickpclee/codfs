7f71eecadba1fac4c3f4315c63e8f2cfeb249cdd
chintran <chintran@live.cn>
2012-09-04 12:06:18 +0800

    restructured to 3-level thread pool
    Former-commit-id: d5e3efc1644ecba5cba84954341b5347293fb383

--- trunk/run.sh
+++ trunk/run.sh
@@ -3,7 +3,7 @@ for((i=0; i < $1; i++));
 do
 	echo "run number: $i";
 	echo "UPLOADING..."
-	./CLIENT clientconfig.xml upload testfile;
+	./CLIENT clientconfig.xml upload testfile_large;
 	
 	echo "DOWNLOADING..."
 	./CLIENT clientconfig.xml download $2 "test_$i";
