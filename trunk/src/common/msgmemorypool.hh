cf01c9f69d33e7919315192a94527cac6f991907
Wilson Li <windkithk@gmail.com>
2012-09-11 14:28:11 +0800

    [COMMON] Message Pool Now Allocate Same Size of Memory for All Message Types
    Former-commit-id: 911e4ef5528d2ad67e6ef09f718d0f06ee20d365

---
+++ trunk/src/common/msgmemorypool.hh
@@ -0,0 +1,22 @@
+#ifndef __MSGMEMORYPOOL_HH__
+#define __MSGMEMORYPOOL_HH__
+
+#include "memorypool.hh"
+
+const apr_size_t MSG_POOL_MAX_FREE_SIZE = 1*1024*1024;
+/**
+ * Provide a memory pool for optimizing frequent malloc / free calls
+ * TODO: A dummy memory pool using singleton pattern for now
+ * TODO: Should be thread-safe in c++11 (verification needed)
+ * Singleton Reference: http://stackoverflow.com/questions/1008019/c-singleton-design-pattern
+ */
+
+class MsgMemoryPool : public MemoryPool {
+	public:
+		MsgMemoryPool();
+		char* poolMalloc(uint32_t size);
+	private:
+		uint32_t _maxMsgSize;
+};
+
+#endif
