dba56006a3c18a80e00e523915cc985b71514d82
Jeremy Chan <cwchan@cse.cuhk.edu.hk>
2012-09-18 14:17:24 +0800

    added back rscoding.cc and rscoding.hh

---
+++ trunk/src/coding/rscoding.hh
@@ -0,0 +1,30 @@
+#ifndef __RS_CODING_HH__
+#define __RS_CODING_HH__
+
+#include "coding.hh"
+
+class RSCoding: public Coding {
+public:
+
+	RSCoding();
+	~RSCoding();
+
+	vector<struct SegmentData> encode(struct ObjectData objectData,
+			string setting);
+
+	struct ObjectData decode(vector<struct SegmentData> &segmentData,
+			vector<uint32_t> &requiredSegments, uint32_t objectSize,
+			string setting);
+
+	vector<uint32_t> getRequiredSegmentIds(string setting,
+			vector<bool> secondaryOsdStatus);
+
+	static string generateSetting(uint32_t k, uint32_t m, uint32_t w) {
+		return to_string(k)+":"+to_string(m)+":"+to_string(w);
+	}
+
+private:
+	vector<uint32_t> getParams(string setting);
+};
+
+#endif
