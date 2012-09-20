c26487a10bd3042147ee0e05ad6d17bb040677fe
Jeremy Chan <cwchan@cse.cuhk.edu.hk>
2012-09-20 13:43:43 +0800

    open empty functions for all FUSE functions in client_fuse.cc
    Former-commit-id: 1a3633aab040f326cec191991549104e1a1d04df

--- trunk/src/fuse/client_fuse.cc
+++ trunk/src/fuse/client_fuse.cc
@@ -31,43 +31,41 @@ uint32_t _clientId = 51000;
 mutex fileInfoCacheMutex;
 mutex _objectProcessingMutex;
 
-unordered_map <uint32_t, FileDataCache*> _fileDataCache;
-unordered_map <uint64_t, unique_lock<mutex> > _objectProcessing;
-unordered_map <uint32_t, struct FileMetaData> _fileInfoCache;
-unordered_map <string, uint32_t> _fileIdCache;
+unordered_map<uint32_t, FileDataCache*> _fileDataCache;
+unordered_map<uint64_t, unique_lock<mutex> > _objectProcessing;
+unordered_map<uint32_t, struct FileMetaData> _fileInfoCache;
+unordered_map<string, uint32_t> _fileIdCache;
 
 thread garbageCollectionThread;
 thread receiveThread;
 thread sendThread;
 
-struct FileMetaData getAndCacheFileInfo (string filePath)
-{
+struct FileMetaData getAndCacheFileInfo(string filePath) {
 	struct FileMetaData fileMetaData;
 	uint32_t fileId;
 	lock_guard<mutex> lk(fileInfoCacheMutex);
-	if(_fileIdCache.count(filePath) == 0) {
+	if (_fileIdCache.count(filePath) == 0) {
 		fileMetaData = _clientCommunicator->getFileInfo(_clientId, filePath);
 		_fileIdCache[filePath] = fileMetaData._id;
 		_fileInfoCache[fileMetaData._id] = fileMetaData;
 		fileId = fileMetaData._id;
-	}
-	else {
+	} else {
 		fileId = _fileIdCache[filePath];
-		debug("File Info Cache Found %s [%" PRIu32 "]\n",filePath.c_str(),fileId);
+		debug("File Info Cache Found %s [%" PRIu32 "]\n",
+				filePath.c_str(), fileId);
 	}
 	return _fileInfoCache[fileId];
 }
 
-struct FileMetaData getAndCacheFileInfo (uint32_t fileId)
-{
+struct FileMetaData getAndCacheFileInfo(uint32_t fileId) {
 	struct FileMetaData fileMetaData;
 	lock_guard<mutex> lk(fileInfoCacheMutex);
-	if(_fileInfoCache.count(fileId) == 0) {
+	if (_fileInfoCache.count(fileId) == 0) {
 		fileMetaData = _clientCommunicator->getFileInfo(_clientId, fileId);
 		_fileIdCache[fileMetaData._path] = fileId;
 		_fileInfoCache[fileId] = fileMetaData;
 	}
-	debug("File Info Cache Found [%" PRIu32 "]\n",fileId);
+	debug("File Info Cache Found [%" PRIu32 "]\n", fileId);
 	return _fileInfoCache[fileId];
 
 }
@@ -86,8 +84,7 @@ void startReceiveThread(Communicator* _clientCommunicator) {
 
 }
 
-void* ncvfs_init(struct fuse_conn_info *conn)
-{
+void* ncvfs_init(struct fuse_conn_info *conn) {
 	_clientCommunicator->createServerSocket();
 
 	// 1. Garbage Collection Thread
@@ -109,17 +106,16 @@ void* ncvfs_init(struct fuse_conn_info *conn)
 	return NULL;
 }
 
-static int ncvfs_getattr(const char *path, struct stat *stbuf)
-{
+static int ncvfs_getattr(const char *path, struct stat *stbuf) {
 
-	if(strcmp(path,"/") == 0) {
+	if (strcmp(path, "/") == 0) {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_uid = getuid();
 		stbuf->st_gid = getgid();
 		return 0;
 	}
 	struct FileMetaData fileMetaData = getAndCacheFileInfo(path);
-	if(fileMetaData._id == 0) {
+	if (fileMetaData._id == 0) {
 		_fileIdCache.erase(path);
 		_fileInfoCache.erase(fileMetaData._id);
 		return -ENOENT;
@@ -136,42 +132,44 @@ static int ncvfs_getattr(const char *path, struct stat *stbuf)
 	return 0;
 }
 
-static int ncvfs_open(const char *path, struct fuse_file_info *fi)
-{
+static int ncvfs_open(const char *path, struct fuse_file_info *fi) {
 	struct FileMetaData fileMetaData = getAndCacheFileInfo(path);
 	fi->fh = fileMetaData._id;
 	return 0;
 }
 
-static int ncvfs_create(const char * path, mode_t mode, struct fuse_file_info *fi)
-{
-	uint32_t objectCount = configLayer->getConfigInt("Fuse>PreallocateObjectNumber");
-	uint32_t objectSize = configLayer->getConfigInt("Storage>ObjectSize") * 1024;
-	struct FileMetaData fileMetaData = _clientCommunicator->uploadFile(_clientId, path, 0, objectCount, codingScheme, codingSetting); 
+static int ncvfs_create(const char * path, mode_t mode,
+		struct fuse_file_info *fi) {
+	uint32_t objectCount = configLayer->getConfigInt(
+			"Fuse>PreallocateObjectNumber");
+	uint32_t objectSize = configLayer->getConfigInt("Storage>ObjectSize")
+			* 1024;
+	struct FileMetaData fileMetaData = _clientCommunicator->uploadFile(
+			_clientId, path, 0, objectCount, codingScheme, codingSetting);
 	_fileIdCache[path] = fileMetaData._id;
 	_fileInfoCache[fileMetaData._id] = fileMetaData;
-	_fileDataCache[fileMetaData._id] = new FileDataCache(fileMetaData, objectSize);
+	_fileDataCache[fileMetaData._id] = new FileDataCache(fileMetaData,
+			objectSize);
 	fi->fh = fileMetaData._id;
 	return 0;
 }
 
-static int ncvfs_read(const char *path, char *buf, size_t size,
-		     off_t offset, struct fuse_file_info *fi)
-{
+static int ncvfs_read(const char *path, char *buf, size_t size, off_t offset,
+		struct fuse_file_info *fi) {
 	struct FileMetaData fileMetaData = getAndCacheFileInfo(fi->fh);
 
-	if((offset + size) > fileMetaData._size)
+	if ((offset + size) > fileMetaData._size)
 		return 0;
-	
+
 	ClientStorageModule* storageModule = client->getStorageModule();
 
-	uint64_t objectSize = storageModule->getObjectSize(); 
+	uint64_t objectSize = storageModule->getObjectSize();
 	uint32_t startObjectNum = offset / objectSize;
 	uint32_t endObjectNum = (offset + size) / objectSize;
-	if(((offset + size) % objectSize) == 0)
+	if (((offset + size) % objectSize) == 0)
 		--endObjectNum;
 	uint64_t byteWritten = 0;
-	for(uint32_t i = startObjectNum; i <= endObjectNum; ++i){
+	for (uint32_t i = startObjectNum; i <= endObjectNum; ++i) {
 		uint64_t objectId = fileMetaData._objectList[i];
 		uint32_t componentId = fileMetaData._primaryList[i];
 		uint32_t sockfd = _clientCommunicator->getSockfdFromId(componentId);
@@ -179,10 +177,10 @@ static int ncvfs_read(const char *path, char *buf, size_t size,
 		struct ObjectTransferCache objectCache;
 		{
 			lock_guard<mutex> lk(_objectProcessingMutex);
-			objectCache = client->getObject(_clientId, sockfd, objectId); 
+			objectCache = client->getObject(_clientId, sockfd, objectId);
 		}
-		uint64_t copySize = min(objectSize,size - byteWritten);
-		copySize = min(copySize, (i+1) * objectSize - (offset + byteWritten));
+		uint64_t copySize = min(objectSize, size - byteWritten);
+		copySize = min(copySize, (i + 1) * objectSize - (offset + byteWritten));
 		uint64_t objectOffset = (offset + byteWritten) - i * objectSize;
 		memcpy(buf + byteWritten, objectCache.buf + objectOffset, copySize);
 		byteWritten += copySize;
@@ -193,23 +191,21 @@ static int ncvfs_read(const char *path, char *buf, size_t size,
 }
 
 static int ncvfs_write(const char *path, const char *buf, size_t size,
-		      off_t offset, struct fuse_file_info *fi)
-{
+		off_t offset, struct fuse_file_info *fi) {
 	(void) buf;
 	(void) offset;
 	(void) fi;
 
 //	if(strcmp(path, "/") != 0)
 //		return -ENOENT;
-	
-	_fileDataCache[fi->fh]->write(buf,size,offset);
+
+	_fileDataCache[fi->fh]->write(buf, size, offset);
 
 	return size;
 }
 
-static int ncvfs_release(const char* path, struct fuse_file_info *fi)
-{
-	debug("Release %s [%" PRIu32 "]\n",path,(uint32_t)fi->fh);
+static int ncvfs_release(const char* path, struct fuse_file_info *fi) {
+	debug("Release %s [%" PRIu32 "]\n", path, (uint32_t)fi->fh);
 	delete _fileDataCache[fi->fh];
 	_fileDataCache.erase(fi->fh);
 	_fileInfoCache.erase(fi->fh);
@@ -217,32 +213,203 @@ static int ncvfs_release(const char* path, struct fuse_file_info *fi)
 	return 0;
 }
 
-void ncvfs_destroy(void* userdata)
-{
+void ncvfs_destroy(void* userdata) {
 	//garbageCollectionThread.join();
 	//receiveThread.join();
 	//sendThread.join();
+	return;
 }
 
-struct ncvfs_fuse_operations: fuse_operations
-{
-	ncvfs_fuse_operations ()
-	{
-		init	= ncvfs_init;
+int ncvfs_chmod(const char *path, mode_t mode) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_chown(const char *path, uid_t uid, gid_t gid) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_utime(const char *path, struct utimbuf *ubuf) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		off_t offset, struct fuse_file_info *fi) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_readlink(const char *path, char *link, size_t size) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_mknod(const char *path, mode_t mode, dev_t dev) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_mkdir(const char *path, mode_t mode) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_unlink(const char *path) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_rmdir(const char *path) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+
+}
+
+int ncvfs_symlink(const char *path, const char *link) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+
+}
+
+int ncvfs_rename(const char *path, const char *newpath) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+
+}
+
+int ncvfs_link(const char *path, const char *newpath) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+
+}
+
+int ncvfs_truncate(const char *path, off_t newsize) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+
+}
+
+int ncvfs_statfs(const char *path, struct statvfs *statv) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+
+}
+
+int ncvfs_flush(const char *path, struct fuse_file_info *fi) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+
+}
+
+int ncvfs_fsync(const char *path, int datasync, struct fuse_file_info *fi) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_setxattr(const char *path, const char *name, const char *value,
+		size_t size, int flags) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_getxattr(const char *path, const char *name, char *value,
+		size_t size) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_listxattr(const char *path, char *list, size_t size) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_removexattr(const char *path, const char *name) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_opendir(const char *path, struct fuse_file_info *fi) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		off_t offset) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_releasedir(const char *path, struct fuse_file_info *fi) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_fsyncdir(const char *path, int datasync, struct fuse_file_info *fi) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_access(const char *path, int mask) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_ftruncate(const char *path, off_t offset, struct fuse_file_info *fi) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+int ncvfs_fgetattr(const char *path, struct stat *statbuf,
+		struct fuse_file_info *fi) {
+	debug_cyan ("%s\n", "not implemented");
+	return 0;
+}
+
+struct ncvfs_fuse_operations: fuse_operations {
+	ncvfs_fuse_operations() {
 		getattr = ncvfs_getattr;
-		open	= ncvfs_open;
-		read	= ncvfs_read;
-		write	= ncvfs_write;
-		create	= ncvfs_create;
-		destroy = ncvfs_destroy;
+		readlink = ncvfs_readlink;
+		getdir = NULL;
+		mknod = ncvfs_mknod;
+		mkdir = ncvfs_mkdir;
+		unlink = ncvfs_unlink;
+		rmdir = ncvfs_rmdir;
+		symlink = ncvfs_symlink;
+		rename = ncvfs_rename;
+		link = ncvfs_link;
+		chmod = ncvfs_chmod;
+		chown = ncvfs_chown;
+		truncate = ncvfs_truncate;
+		utime = ncvfs_utime;
+		open = ncvfs_open;
+		read = ncvfs_read;
+		write = ncvfs_write;
+		statfs = ncvfs_statfs;
+		flush = ncvfs_flush;
 		release = ncvfs_release;
+		fsync = ncvfs_fsync;
+		setxattr = ncvfs_setxattr;
+		getxattr = ncvfs_getxattr;
+		listxattr = ncvfs_listxattr;
+		removexattr = ncvfs_removexattr;
+		opendir = ncvfs_opendir;
+		readdir = ncvfs_readdir;
+		releasedir = ncvfs_releasedir;
+		fsyncdir = ncvfs_fsyncdir;
+		init = ncvfs_init;
+		destroy = ncvfs_destroy;
+		access = ncvfs_access;
+		create = ncvfs_create;
+		ftruncate = ncvfs_ftruncate;
+		fgetattr = ncvfs_fgetattr;
 	}
 };
 
 static struct ncvfs_fuse_operations ncvfs_oper;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	configLayer = new ConfigLayer("clientconfig.xml");
 	client = new Client(_clientId);
 	//fileDataModule = new FileDataModule();
