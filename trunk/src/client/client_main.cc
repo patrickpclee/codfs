c73e23013e6b754c80b33fde22c114ca5095bc98
chintran <chintran@live.cn>
2012-11-15 16:19:36 +0800

    using boost_program_options
    Former-commit-id: e420f61fa8937b24bf1c13b4565a6dbbfb4b8d6a

--- trunk/src/client/client_main.cc
+++ trunk/src/client/client_main.cc
@@ -8,6 +8,8 @@
 #include "../coding/raid5coding.hh"
 #include "../coding/rscoding.hh"
 #include "../../lib/logger.hh"
+#include "boost/program_options.hpp"
+
 
 using namespace std;
 
@@ -41,11 +43,11 @@ void startReceiveThread(Communicator* communicator) {
 
 int main(int argc, char *argv[]) {
 
-	if (argc < 4 || argc > 5) {
-		cout << "Upload: ./CLIENT [CLIENT ID] upload [SRC]" << endl;
-		cout << "Download: ./CLIENT [CLIENT ID] download [FILEID] [DST]" << endl;
-		exit(-1);
-	}
+//	if (argc < 4 || argc > 5) {
+//		cout << "Upload: ./CLIENT [CLIENT ID] upload [SRC]" << endl;
+//		cout << "Download: ./CLIENT [CLIENT ID] download [FILEID] [DST]" << endl;
+//		exit(-1);
+//	}
 
 	// handle signal for profiler
 	signal(SIGINT, sighandler);
@@ -76,9 +78,94 @@ int main(int argc, char *argv[]) {
 	communicator->setComponentType(CLIENT);
 	
 	//communicator->connectAllComponents();
-	communicator->connectToMds();
+/*	communicator->connectToMds();
 	communicator->connectToMonitor();
 	communicator->getOsdListAndConnect();
+*/
+	try{
+	    namespace po = boost::program_options;
+	    po::options_description desc("Options");
+	    desc.add_options()
+	      ("help", "Print help messages")
+	      ("upload", "file upload")
+	      ("download", "file download")
+	      ("raid0", "raid0 coding")
+	      ("raid1", "raid0 coding")
+	      ("raid5", "raid0 coding")
+	      ("rs", "RS coding")
+	      ("n", po::value<int>(), "number of replications")
+	    ("k", po::value<int>(), "number of rs_k")
+	    ("m", po::value<int>(), "number of rs_m")
+	    ("w", po::value<int>(), "number of rs_w")
+	    ("i",po::value<int>(), "file ID")
+	    ("d",po::value<string>(), "destination file path")
+	    ("f",po::value<string>(), "file path");
+
+	    po::variables_map vm;
+	    try
+	    {
+	      po::store(po::parse_command_line(argc, argv, desc), vm); // can throw
+
+	      /** --help option
+	       */
+	      if ( vm.count("help")  )
+	      {
+	        std::cout << "Basic Command Line Parameter App" << std::endl
+	                  << desc << std::endl;
+	      }
+
+	      if ( vm.count("upload")  )
+	      {
+
+	    	  	  	  CodingScheme codingScheme;
+	    	  	  	  string codingSetting;
+	    	  	  	 if(vm.count("raid0") ){
+	    	  	  		    	  	  		  	  codingScheme = RAID0_CODING;
+	    	  	  		    	  	  		std::cout << vm["n"].as<int>() << std::endl;
+//	    	  	  		    	  	  		  S	codingSetting = Raid0Coding::generateSetting(vm["n"].as<uint32_t>());
+	    	  	  		    	  	  	  }
+
+	    	  	  	  if(vm.count("raid1") ){
+	    	  	  		  	  codingScheme = RAID1_CODING;
+	    	  	  		  	codingSetting = Raid1Coding::generateSetting(vm["n"].as<int>());
+	    	  	  	  }
+	    	  	  	 if(vm.count("raid5") ){
+	    	  	  		    	  	  		  	  codingScheme = RAID5_CODING;
+	    	  	  		    	  	  		  	codingSetting = Raid5Coding::generateSetting(vm["n"].as<int>());
+	    	  	  		    	  	  	  }
+
+	    	  	  	 if(vm.count("rs") ){
+	    	  	  		    	  	  		  	  codingScheme = RS_CODING;
+	    	  	  		    	  	  		  	codingSetting = RSCoding::generateSetting(vm["k"].as<int>(),vm["m"].as<int>(),vm["w"].as<int>());
+	    	  	  		    	  	  	  }
+
+	    	  	  	  client->uploadFileRequest(vm["f"].as<string>(), codingScheme, codingSetting);
+
+	      	      }
+
+	      if ( vm.count("download")  )
+	      	      {
+	    	  	  	  client->downloadFileRequest(vm["i"].as<int>(), vm["d"].as<string>());
+
+	      	      }
+	      po::notify(vm);
+	       // throws on error, so do after help in case
+	                      // there are any problems
+	    }
+	    catch(po::error& e)
+	    {
+	      std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+	      std::cerr << desc << std::endl;
+	      return 1;
+	    }
+	  }
+	  catch(std::exception& e)
+	  {
+	    std::cerr << "Unhandled Exception reached the top of main: "
+	              << e.what() << ", application will now exit" << std::endl;
+	    return 2;
+
+	  }
 
 
 	////////////////////// TEST FUNCTIONS ////////////////////////////
@@ -94,12 +181,12 @@ int main(int argc, char *argv[]) {
 	CodingScheme codingScheme = RAID0_CODING;
 	string codingSetting = Raid0Coding::generateSetting(raid0_n);
 	 */
-
+	  /*
 	// RAID 1
 	const uint32_t raid1_n = 1;
 	CodingScheme codingScheme = RAID1_CODING;
 	string codingSetting = Raid1Coding::generateSetting(raid1_n);
-
+*/
 	/*
 
 	// RAID 5
@@ -116,11 +203,11 @@ int main(int argc, char *argv[]) {
 	string codingSetting = RSCoding::generateSetting(rs_k, rs_m, rs_w);
 	*/
 
-	if (strncmp(argv[2], "upload", 6) == 0) {
-		client->uploadFileRequest(argv[3], codingScheme, codingSetting);
-	} else {
-		client->downloadFileRequest(atoi(argv[3]), argv[4]);
-	}
+//	if (strncmp(argv[2], "upload", 6) == 0) {
+//		client->uploadFileRequest(argv[3], codingScheme, codingSetting);
+//	} else {
+//		client->downloadFileRequest(atoi(argv[3]), argv[4]);
+//	}
 
 	/*
 
