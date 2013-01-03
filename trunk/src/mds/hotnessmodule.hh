1ed523258264f5df1da05ce02875b78f7870e0b9
DING QIAN <essyding@localhost.localdomain>
2013-01-03 13:01:52 +0800

    [MDS] Start building hotnessmodule
    Former-commit-id: ecc91480e40e58952c5f672c471bee740b6edf25

---
+++ trunk/src/mds/hotnessmodule.hh
@@ -0,0 +1,22 @@
+#ifndef __HOTNESS_MODULE_HH__
+#define __HOTNESS_MODULE_HH__
+
+#include <stdint.h>
+#include <vector>
+using namespace std;
+
+#include "configmetadatamodule.hh"
+#include "filemetadatamodule.hh"
+#include "segmentmetadatamodule.hh"
+#include "osdmetadatamodule.hh"
+#include "../common/metadata.hh"
+
+class HotnessModule {
+public:
+	HotnessModule();
+
+private:
+
+};
+
+#endif
