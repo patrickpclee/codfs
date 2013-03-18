a916707770d1acc536a298f9c3ebcfc3ae9d26d0
Wilson Li <windkit@cuhk.edu.hk>
2013-03-18 17:20:49 +0800

    [FUSE] Completed Create File
    Former-commit-id: 840a572ff8cebb341cf3d67c378c74d4f146ee9c

---
+++ trunk/src/fuse/filemetadatacache.cc
@@ -0,0 +1,27 @@
+#include "filemetadatacache.hh"
+
+uint32_t FileMetaDataCache::path2Id(string path) {
+	return _fileIdCache.at(path);
+}
+string FileMetaDataCache::id2Path(uint32_t id) {
+	return _metaDataCache.at(id)._path;
+}
+
+struct FileMetaData FileMetaDataCache::getMetaData(uint32_t id) {
+	return _metaDataCache.at(id);
+}
+
+void FileMetaDataCache::saveMetaData(struct FileMetaData fileMetaData) {
+	uint32_t id = fileMetaData._id;
+	string path = fileMetaData._path;
+	_fileIdCache[path] = id;
+	_metaDataCache[id] = fileMetaData;
+	return ;
+}
+
+void FileMetaDataCache::removeMetaData(uint32_t id) {
+	string path = id2Path(id);
+	_fileIdCache.erase(path);
+	_metaDataCache.erase(id);
+	return ;
+}
