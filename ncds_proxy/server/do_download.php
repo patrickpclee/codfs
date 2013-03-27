191d1aaa30056bfee6a83438b0533168d23fb60e
Jeremy Chan <cwchan@cse.cuhk.edu.hk>
2013-03-27 17:02:10 +0800

    [PROXY] improved code

--- ncds_proxy/server/do_download.php
+++ ncds_proxy/server/do_download.php
@@ -1,19 +1,30 @@
 <?php
 
+require_once('KLogger.php');
+
+$log = new KLogger ( "log.txt" , KLogger::DEBUG );
+
 if (empty($_GET['fileid'])){
     return false;
 } else{
     $fileid = $_GET['fileid'];
 }
 
+$log->LogInfo("Download: $fileid");
+
 // log file
 $client_no = rand();
 $logfile = uniqid(rand(), true) . '.log';
 $outfile = uniqid(rand(), true) . '.out';
 system ("cd ../bin; ./CLIENT_p -i $client_no -a download -f $fileid -t $outfile > $logfile 2>&1");
+
+$log->LogInfo("Downloaded from NCDS FileID: $fileid");
+
 send_file("../bin/$outfile", $fileid);
 unlink("../bin/$outfile");
 unlink("../bin/$logfile");
+
+$log->LogInfo("Sent to Client FileID: $fileid");
 
 function send_file($file, $fileid) {
     if (file_exists($file)) {
