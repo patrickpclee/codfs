b8181e47a65d4f5245a8c186f7569d1da7927c57
Jeremy Chan <cwchan@cse.cuhk.edu.hk>
2013-05-06 16:11:48 +0800

    [PROXY] multi file upload
    Former-commit-id: 33b15dda2af5d7bca48a86f718fd43b54c5cfca1

--- ncds_proxy/server/do_upload.php
+++ ncds_proxy/server/do_upload.php
@@ -1,38 +1,172 @@
 <?php
+/**
+ * upload.php
+ *
+ * Copyright 2009, Moxiecode Systems AB
+ * Released under GPL License.
+ *
+ * License: http://www.plupload.com/license
+ * Contributing: http://www.plupload.com/contributing
+ */
 
-$backup_location = "physics@ncds1:/disk1/physics_backup";
+// HTTP headers for no cache etc
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+// Settings
+$targetDir = ini_get('upload_tmp_dir') ? ini_get('upload_tmp_dir') : sys_get_temp_dir();
+//$targetDir = 'tmp_uploads';
+
+$cleanupTargetDir = true; // Remove old files
+$maxFileAge = 5 * 3600; // Temp file age in seconds
+
+// 15 minutes execution time
+@set_time_limit(15 * 60);
+
+// Uncomment this one to fake upload time
+// usleep(5000);
+
+// Get parameters
+$chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
+$chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 0;
+$fileName = isset($_REQUEST["name"]) ? $_REQUEST["name"] : '';
+
+// Clean the fileName for security reasons
+$fileName = preg_replace('/[^\w\._]+/', '_', $fileName);
+
+// Make sure the fileName is unique but only if chunking is disabled
+if ($chunks < 2 && file_exists($targetDir . DIRECTORY_SEPARATOR . $fileName)) {
+	$ext = strrpos($fileName, '.');
+	$fileName_a = substr($fileName, 0, $ext);
+	$fileName_b = substr($fileName, $ext);
+
+	$count = 1;
+	while (file_exists($targetDir . DIRECTORY_SEPARATOR . $fileName_a . '_' . $count . $fileName_b))
+		$count++;
+
+	$fileName = $fileName_a . '_' . $count . $fileName_b;
+}
+
+$filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
+
+// Create target dir
+if (!file_exists($targetDir))
+	@mkdir($targetDir);
+
+// Remove old temp files	
+if ($cleanupTargetDir) {
+	if (is_dir($targetDir) && ($dir = opendir($targetDir))) {
+		while (($file = readdir($dir)) !== false) {
+			$tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
+
+			// Remove temp file if it is older than the max age and is not the current file
+			if (preg_match('/\.part$/', $file) && (filemtime($tmpfilePath) < time() - $maxFileAge) && ($tmpfilePath != "{$filePath}.part")) {
+				@unlink($tmpfilePath);
+			}
+		}
+		closedir($dir);
+	} else {
+		die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
+	}
+}	
+
+// Look for the content type header
+if (isset($_SERVER["HTTP_CONTENT_TYPE"]))
+	$contentType = $_SERVER["HTTP_CONTENT_TYPE"];
+
+if (isset($_SERVER["CONTENT_TYPE"]))
+	$contentType = $_SERVER["CONTENT_TYPE"];
+
+// Handle non multipart uploads older WebKit versions didn't support multipart in HTML5
+if (strpos($contentType, "multipart") !== false) {
+	if (isset($_FILES['file']['tmp_name']) && is_uploaded_file($_FILES['file']['tmp_name'])) {
+		// Open temp file
+		$out = @fopen("{$filePath}.part", $chunk == 0 ? "wb" : "ab");
+		if ($out) {
+			// Read binary input stream and append it to temp file
+			$in = @fopen($_FILES['file']['tmp_name'], "rb");
+
+			if ($in) {
+				while ($buff = fread($in, 4096))
+					fwrite($out, $buff);
+			} else
+				die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+			@fclose($in);
+			@fclose($out);
+			@unlink($_FILES['file']['tmp_name']);
+		} else
+			die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+	} else
+		die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+} else {
+	// Open temp file
+	$out = @fopen("{$filePath}.part", $chunk == 0 ? "wb" : "ab");
+	if ($out) {
+		// Read binary input stream and append it to temp file
+		$in = @fopen("php://input", "rb");
+
+		if ($in) {
+			while ($buff = fread($in, 4096))
+				fwrite($out, $buff);
+		} else
+			die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+
+		@fclose($in);
+		@fclose($out);
+	} else
+		die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+}
+
+// Check if file has been uploaded
+if (!$chunks || $chunk == $chunks - 1) {
+	// Strip the temp .part suffix off 
+	rename("{$filePath}.part", $filePath);
+}
+
+// 
+// Push to NCDS
+//
 
 require_once('uploaded.class.php');
 require_once('KLogger.php');
-
+$backup_location = "physics@ncds1:/disk1/physics_backup";
 $log = new KLogger ( "log.txt" , KLogger::DEBUG );
 
-if (isset($_POST['submit'])) {
-    if (!empty ($_POST['path'])) {
-        $path = str_replace('/', '@', $_POST['path']);
-    } else {
-        $path = $string = str_replace(' ', '_', microtime());
+if (isset($_REQUEST['path']) && !empty ($_REQUEST['path'])) {
+
+    // prepare destination name
+    $dstpath = str_replace('/', '@', $_REQUEST['path']);
+    if (substr($dstpath, -1) != "@") {
+        $dstpath = $dstpath . "@";
     }
+    $dstpath = $dstpath . $fileName;
 
-    $log->LogInfo("Upload: $path");
+    $log->LogInfo("Upload: $filePath -> $dstpath");
 
-    $file = new Uploaded('upload_file');
-    $fileid = $file->uploadToNCDS ($path);
+    $file = new Uploaded($filePath);
+    $fileid = $file->uploadToNCDS ($dstpath);
 
-    $log->LogInfo("Uploaded to NCDS Path: $path FileID: $fileid");
+    $log->LogInfo("Uploaded to NCDS Path: $dstpath FileID: $fileid");
 
-    if ($file->makeBackup($path, $backup_location)) {
-        print "Replication Successful <br />";
-        $log->LogInfo("Backup Successful: $path Location: $backup_location");
+    if ($file->makeBackup($dstpath, $backup_location)) {
+        $log->LogInfo("Backup Successful: $dstpath Location: $backup_location");
     } else {
-        $log->LogError("Backup Error: $path Location: $backup_location");
+        $log->LogError("Backup Error: $dstpath Location: $backup_location");
     }
 
     if ($fileid) {
         $file->saveMappingToDb();
-        $log->LogInfo("Mapping Saved: $path FileId: $fileid");
-    } else {
-        print "Upload Failed <br />";
+        $log->LogInfo("Mapping Saved: $dstpath FileId: $fileid");
     }
+
+    // remove tmp
+    unlink ($filePath);
+
+} else {
+    die('{"jsonrpc" : "2.0", "error" : {"code": 300, "message": "Failed to get destination path."}, "id" : "id"}');
 }
-?>
+
+die('{"jsonrpc" : "2.0", "result" : null, "id" : "id"}');
--- ncds_proxy/server/uploaded.class.php
+++ ncds_proxy/server/uploaded.class.php
@@ -1,88 +1,72 @@
 <?php
-    error_reporting(E_ALL);
-    class Uploaded {
-        private $field_name = '';
-        // $field_name is the name of the input in uploading form
-        public function __construct($field_name) {
-            $this -> field_name = $field_name;
-        }
-
-        // invoke CLIENT to upload the file to NCDS
-        public function uploadToNCDS ($path) {
-
-            print "Path: $path <br />";
-
-            // CLIENT parameters
-            $tmpname = $_FILES[$this->field_name]['tmp_name'];
-            $client_no = rand();
-            $scheme = 'evenodd';
-            $n = '7';
-
-            // not used
-            $k = '6';
-            $m = '2';
-            $w = '8';
-
-            // log file
-            $logfile = uniqid(rand(), true) . '.log';
-
-            if (is_uploaded_file($tmpname)) {
-
-                system ("cd ../bin; ./CLIENT_p -i $client_no -a upload -c $scheme -n $n -k $k -m $m -w $w -t $tmpname > $logfile 2>&1");
-
-                $log_content = file_get_contents("../bin/$logfile", true);
-
-                // check successful
-                print "File ID: <br />";
-                preg_match_all('/Upload .+ Done \[(.+)\]/', $log_content, $matches);
-                $fileid = $matches[1][0];
-                print $fileid;
-
-                print "<br />";
-
-                print "Last Successful Upload: <br />";
-                preg_match_all("/.+transferred in.+/", $log_content, $matches);
-                print $matches[0][0];
-
-                print "<br />";
-
-                unlink("../bin/$logfile");
-
-                $this->path = $path;
-                $this->fileid = $fileid;
-                return $fileid;
-
-            }
-            return false;
-        }
-
-        public function makeBackup ($path, $backup_location) {
-
-            // CLIENT parameters
-            $tmpname = $_FILES[$this->field_name]['tmp_name'];
-
-            if (is_uploaded_file($tmpname)) {
-
-                system ("scp $tmpname $backup_location/$path");
-
-                return true;
-            }
-            return false;
-        }
-
-        public function saveMappingToDb () {
-            try{
-                $db = new PDO('sqlite:filelist.sqlite3');
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $db->exec("DELETE FROM FILELIST WHERE PATH = '$this->path';");
-                $db->exec("INSERT INTO FILELIST (PATH, FILEID) VALUES ('$this->path', $this->fileid);");
-                print 'Saved to DB <br />';
-                $db = null;
-            } catch(PDOException $e) {
-                print 'Exception : '.$e->getMessage();
-            }
-
-        }
-        
+error_reporting(E_ALL);
+class Uploaded {
+    private $tmppath = '';
+    public function __construct($tmppath) {
+        $this->tmppath = $tmppath;
     }
+
+    // invoke CLIENT to upload the file to NCDS
+    public function uploadToNCDS ($path) {
+
+        print "Path: $path <br />";
+
+        // CLIENT parameters
+        $client_no = rand();
+        $scheme = 'evenodd';
+        $n = '7';
+
+        // not used
+        $k = '6';
+        $m = '2';
+        $w = '8';
+
+        // log file
+        $logfile = uniqid(rand(), true) . '.log';
+
+        system ("cd ../bin; ./CLIENT_p -i $client_no -a upload -c $scheme -n $n -k $k -m $m -w $w -t $this->tmppath > $logfile 2>&1");
+
+        $log_content = file_get_contents("../bin/$logfile", true);
+
+        // check successful
+        print "File ID: <br />";
+        preg_match_all('/Upload .+ Done \[(.+)\]/', $log_content, $matches);
+        $fileid = $matches[1][0];
+        print $fileid;
+
+        print "<br />";
+
+        print "Last Successful Upload: <br />";
+        preg_match_all("/.+transferred in.+/", $log_content, $matches);
+        print $matches[0][0];
+
+        print "<br />";
+
+        //unlink("../bin/$logfile");
+
+        $this->path = $path;
+        $this->fileid = $fileid;
+        return $fileid;
+    }
+
+    public function makeBackup ($path, $backup_location) {
+        system ("scp $this->tmppath $backup_location/$path");
+        return true;
+    }
+
+    public function saveMappingToDb () {
+        try{
+            $db = new PDO('sqlite:filelist.sqlite3');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $db->exec("DELETE FROM FILELIST WHERE PATH = '$this->path';");
+            $db->exec("INSERT INTO FILELIST (PATH, FILEID) VALUES ('$this->path', $this->fileid);");
+            print 'Saved to DB <br />';
+            $db = null;
+        } catch(PDOException $e) {
+            print 'Exception : '.$e->getMessage();
+        }
+
+    }
+
+}
 ?>
