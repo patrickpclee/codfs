4e018b1c883da5ae9b6e3a53496f5577f303291f
Jeremy Chan <cwchan@cse.cuhk.edu.hk>
2013-05-06 16:11:48 +0800

    [PROXY] multi file upload

--- ncds_proxy/server/upload.php
+++ ncds_proxy/server/upload.php
@@ -2,41 +2,55 @@
    "http://www.w3.org/TR/html4/strict.dtd">
 <HTML>
    <HEAD>
+        <link rel="stylesheet" href="plupload/js/jquery.plupload.queue/css/jquery.plupload.queue.css" type="text/css" media="screen" />
       <TITLE>NCDS File Uploader</TITLE>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script type="text/javascript" src="plupload/js/plupload.js"></script>
+        <script type="text/javascript" src="plupload/js/plupload.html4.js"></script>
+        <script type="text/javascript" src="plupload/js/plupload.html5.js"></script>
+        <script type="text/javascript" src="plupload/js/jquery.plupload.queue/jquery.plupload.queue.js"></script>
         <script>
             $(document).ready(function() {
-                $("#uploadForm").submit(function(){
+
+                $("#pathForm").submit(function(){
                     var isFormValid = true;
 
-                    $("#uploadForm input").each(function(){
-                        if ($.trim($(this).val()).length == 0){
-                            $(this).addClass("highlight");
-                            isFormValid = false;
-                        }
-                        else{
-                            $(this).removeClass("highlight");
-                        }
-                    });
-
-                    if (!isFormValid) alert("Please fill in all the required fields");
-
-                    var str = $("#path").val();
+                    var str = $.trim($("#path").val());
+                    if (str.length == 0) {
+                        isFormValid = false;
+                    }
                     if(/^[a-zA-Z0-9-_.\/]*$/.test(str) == false) {
                         isFormValid = false;
-                        alert('Path string contains illegal characters.');
                     }
 
-                    return isFormValid;
+                    if (!isFormValid) {
+                        alert ("Invalid path");
+                        return false;
+                    }
+
+                    $("#pathForm :input").attr('disabled', true);
+
+                    $("#uploader").pluploadQueue({
+                        // General settings
+                        runtimes : 'html5,html4',
+                            url : 'plupload.php',
+                            multipart_params: {'path': str}
+                    });
+
+                    return false;
                 });
             });
         </script>
    </HEAD>
    <BODY>
-        <form id="uploadForm" enctype="multipart/form-data" method="post" action="do_upload.php">
-            <input id="file" type="file" name="upload_file" /> <br /><br />
-            Target Path: <input id="path" type="text" name="path" /> <br /><br />
-            <button type="submit" name="submit">Upload to NCDS</button>
+
+        <form id="pathForm" action="">
+            Destination Folder on NCDS: <input id="path" type="text" name="path" /> 
+            <button type="submit" name="submit">Set Folder Path</button>
         </form>
+        
+        <br /><br />
+
+        <div id="uploader"></div>
    </BODY>
 </HTML>
