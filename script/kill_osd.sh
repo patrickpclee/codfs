59b1d651e2e7b81099afbf2c26205fbb7bed204b
ncsgroup <ncsgroup@testbed-gateway.(none)>
2013-09-02 16:57:41 +0800

    updated kill_osd.sh
    Former-commit-id: e3c15175ab646896f5a7d03b783ec8869c501d79

--- script/kill_osd.sh
+++ script/kill_osd.sh
@@ -6,6 +6,7 @@ fi
 targetip=192.168.0.$1
 command="ssh -t -t $targetip 'pkill -9 ifstat; pkill -9 MONITOR; pkill -9 CLIENT_FUSE; pkill -9 gdb; pkill -9 MDS; pkill -9 OSD; "
 echo $command
+`$command`
 
 echo "KILLED OSD # $1"
 screen -ls
