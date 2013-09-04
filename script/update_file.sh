550f9955311eba7d8f966dc33b7906e9ece101f7
ncsgroup <ncsgroup@testbed-gateway.(none)>
2013-09-04 15:27:52 +0800

    [script] update_file.sh
    Former-commit-id: bf19c24ace5e3c4241b203a09fe71ff1ff865a53

--- script/update_file.sh
+++ script/update_file.sh
@@ -7,12 +7,15 @@ then
 fi
 
 fuseNum=$1
+filesize=$2
+updatesize=$3
+updatecount=$4
 alive=`cat alive_client | sort | uniq | head -n $fuseNum`
 set $alive
 for target
 do
     targetip=192.168.0.$target
-    command="cd ~/shb118/ncvfs/benchmark; make; ./generate_even_write.py 2000M 4K 4K /home/ncsgroup/shb118/ncvfs/trunk/mountdir/benchmarkfile${target} > even_write; ./doUpdate < even_write"
+    command="cd ~/shb118/ncvfs/benchmark; make; ./generate_even_write.py $filesize $updatesize $updatecount /home/ncsgroup/shb118/ncvfs/trunk/mountdir/benchmarkfile${target} > even_write; ./doUpdate < even_write"
     screen -dm -t UPDATE$target -S UPDATE$target ssh -t -t $targetip $command
 done
 while true
