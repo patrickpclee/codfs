8d4b7a6c70b308f0ee800f09b0e455337fdd3271
ncsgroup <ncsgroup@testbed-gateway.(none)>
2013-09-04 19:29:01 +0800

    use ext2
    Former-commit-id: ebab0b0f7c297d132fd4152eddbf102da9393661

--- script/init_disks.sh
+++ script/init_disks.sh
@@ -7,7 +7,7 @@ do
 	targetip=192.168.0.$target
     uuid=`ssh -t -q root@$targetip "cat /etc/fstab | grep /data | cut -d '=' -f2 | cut -d ' ' -f 1"`
     uuid=`echo -n "$uuid" | sed 's/\r//g'`
-	echo "ssh -t -t root@$targetip 'unlink $osdblock; rm -rf $osdblock; umount /data; mke2fs -T ext4 -U $uuid /dev/sdb1 && mount -a; mkdir -p /data/osd_block; chown ncsgroup:ncsgroup /data/osd_block; ln -s /data/osd_block $osdblock;'" >> initdisks.job
+	echo "ssh -t -t root@$targetip 'unlink $osdblock; rm -rf $osdblock; umount /data; mke2fs -T ext2 -U $uuid /dev/sdb1 && mount -a; mkdir -p /data/osd_block; chown ncsgroup:ncsgroup /data/osd_block; ln -s /data/osd_block $osdblock;'" >> initdisks.job
 done
 parallel --jobs 0 < initdisks.job
 rm initdisks.job
